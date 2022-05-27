96ed8cf23d1aef95d56d48db45a9c2709eede80b
bdairshadi <bdairshadi@gmail.com>
2022-05-28 02:17:59 +0300

    Added image prune + The if statment.

--- Docker-CleanUP.sh
+++ Docker-CleanUP.sh
@@ -4,3 +4,12 @@
 df -h
 # Remove all unused local volumes
 docker volume prune -f
+# Remove unused images
+docker image prune -af
+# Variable that will contain docker list.
+var=$(docker container ls -aq)
+if [ -z "$var" ];
+  then echo "Null";
+  else docker container rm $(docker container ls -aq);
+fi
+echo "done"
